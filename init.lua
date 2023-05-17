afba482fa8f862387c645129b34d441500225df8
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2023-05-17 11:43:46 -0400

    Add script to init.lua to auto-open NvimTree

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
@@ -6,3 +6,14 @@
 --   command = "tabdo wincmd =",
 -- })
 
+autocmd({"BufNewFile", "BufWinEnter"}, {
+  callback = function(args)
+    if vim.fn.expand "%:p" ~= "" then
+      vim.api.nvim_del_autocmd(args.id)
+      vim.cmd "NvimTreeToggle"
+      vim.schedule(function()
+        vim.cmd "wincmd p"
+      end)
+    end
+  end,
+})
