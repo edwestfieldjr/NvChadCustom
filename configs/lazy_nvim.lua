84d57d771fc7b86f7800ca77c0c8721b9f450367
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2023-05-18 10:23:26 -0400

    Add custom configs for lazy.nvim

--- configs/lazy_nvim.lua
+++ configs/lazy_nvim.lua
@@ -1,18 +1,15 @@
 return {
-  -- defaults = { lazy = true },
-
   checker = {
     -- automatically check for plugin updates
     enabled = true,
     concurrency = 1, ---@type number? set to 1 to check for updates very slowly
-    notify = false, -- get a notification when new updates are found
-    frequency = 86400, -- check for updates every hour
+    notify = false, -- will NOT get a notification when new updates are found
+    frequency = 86400, -- check for updates every 24 hours
   },
 
   change_detection = {
     -- automatically check for config file changes and reload the ui
     enabled = true,
-    notify = false, -- get a notification when changes are found
+    notify = false, -- will NOT get a notification when changes are found
   },
-
 }
