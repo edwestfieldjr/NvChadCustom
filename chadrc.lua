d87769cfccbbbffb4db021eef0865e87b5d415ea
Ed Westfield Jr. 西田爱德 <30934590+edwestfieldjr@users.noreply.github.com>
2023-05-22 09:15:43 -0400

    Modified UI for future config developement

--- chadrc.lua
+++ chadrc.lua
@@ -11,17 +11,6 @@ M.ui = {
   hl_override = highlights.override,
   hl_add = highlights.add,
 
-  -- statusline = {
-  -- theme = "minimal", -- default/vscode/vscode_colored/minimal
-  --   -- default/round/block/arrow separators work only for default statusline theme
-  --   -- round and block will work for minimal theme only
-  --   separator_style = "round",
-  --   overriden_modules = function()
-  --     return strftime('%c')
-  --   end,
-  -- },
-
-  -- lazyload it when there are 1+ buffers
   tabufline = {
     show_numbers = false,
     enabled = true,
